71ad95459cfaac1da2609213d339fbdabd6ef267
Marco <marco.faltoni@outlook.it>
2020-11-09 22:27:12 +0100

    Init project and basic structure

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// import styles
+import "./style/app.scss";
+
+// Adding components
+import Player from './components/Player';
+import Song from './components/Song';
+
+
+function App() {
+    return (
+      <div className="App">
+        {/* <h1>A Very Funny Music Player</h1> */}
+        <Song />
+        <Player />
+      </div>
+    );
+}
+
+export default App;
